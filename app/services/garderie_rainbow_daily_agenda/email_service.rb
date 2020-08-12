a27026af15c359a911064b567503fcc8b877875c
Andy Maleh <andy.am@gmail.com>
2020-08-12 00:19:15 -0400

    Added public/private key encryption and storage of preferences

--- app/services/garderie_rainbow_daily_agenda/email_service.rb
+++ app/services/garderie_rainbow_daily_agenda/email_service.rb
@@ -52,18 +52,44 @@ class GarderieRainbowDailyAgenda
       end
     end
     
-    def config_file
-      @config_file ||= File.join(GarderieRainbowDailyAgenda::APP_ROOT, 'config', 'email_service.yml')
+    def rsa
+      @rsa ||= load_rsa
     end
     
-    def save
+    def rsa_private_key_file
+      File.join(APP_ROOT, 'config', '.rsa')
+    end
+    
+    def rsa_private_key
+      File.read(rsa_private_key_file) if File.exist?(rsa_private_key_file)
+    end
+    
+    def load_rsa
+      private_key = rsa_private_key
+      OpenSSL::PKey::RSA.new(private_key || 2048).tap do |loaded_rsa|
+        if private_key.nil?
+          File.write(rsa_private_key_file, loaded_rsa.to_pem) if private_key.nil?
+          FileUtils.chmod(0600, rsa_private_key_file)
+        end
+      end
+    end
+    
+    def config_file
+      @config_file ||= File.join(GarderieRainbowDailyAgenda::APP_ROOT, 'config', 'email_service.yml')
+    end        
+    
+    def save      
       config_yaml = YAML.dump(attributes)
-      File.write(config_file, config_yaml) unless config_yaml.to_s.empty?
+      File.write(config_file, rsa.public_encrypt(config_yaml)) unless config_yaml.to_s.empty?
     end
     
     def load
-      config_yaml = File.read(config_file) if File.exist?(config_file)
+      config_yaml = rsa.private_decrypt(File.read(config_file)) if File.exist?(config_file)
       self.attributes = YAML.load(config_yaml) unless config_yaml.to_s.empty?
+    rescue => e
+      # No Op. Ignore if rsa failed in decryption (someone deleted its key) or file was corrupted.
+      # Just save a new one later on.
+      Glimmer::Config.logger.error e.full_message
     end
   end
 end
